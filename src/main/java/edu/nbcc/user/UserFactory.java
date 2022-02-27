65c79b2ce8f40d1fe11675eadbebda1b71a8266a
arunjoeljohn <97317083+arunjoeljohn@users.noreply.github.com>
2022-02-27 16:02:24 -0400

    update

--- src/main/java/edu/nbcc/user/UserFactory.java
+++ src/main/java/edu/nbcc/user/UserFactory.java
@@ -1,6 +1,13 @@
 package edu.nbcc.user;
 
 public abstract class UserFactory {
+	/**
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param emailAddress
+	 * @return
+	 */
 	  public static IUser createInstance(String firstName, String lastName, String emailAddress) {
 	    return new User(firstName, lastName, emailAddress);
 	  }
